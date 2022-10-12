c826092685ecc772b046e371eccb6606a14a5bef
Nikoloz Shautidze <nickshaut@Nikolozs-MacBook-Pro.local>
2022-10-12 16:22:57 +0300

    refactors todo.service

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -16,35 +16,29 @@ export class TodoService {
     return this.todos;
   }
 
-  getATodo(todoId: string) {
-    const aTodo = this.findTodo(todoId).index;
-    return aTodo;
+  getATodo(todoId: string): Todo {
+    return this.findTodo(todoId).todo;
   }
 
   updateTodo(todoId: string, title: string, text: string): void {
     const { todo, index } = this.findTodo(todoId);
-    if (title) {
-      todo.title = title;
-    }
-    if (text) {
-      todo.text = text;
-    }
+
+    todo.title = title ?? todo.title;
+    todo.text = text ?? todo.text;
 
     this.todos[index] = todo;
   }
 
   private findTodo(todoId: string): { todo: Todo; index: number } {
     const todoIndex = this.todos.findIndex((todo) => todo.id === todoId);
-    if (todoIndex == -1) {
+    if (todoIndex === -1) {
       throw 404;
-    } else {
-      const aTodo = this.todos[todoIndex];
-      return { todo: aTodo, index: todoIndex };
     }
+    return { todo: this.todos[todoIndex], index: todoIndex };
   }
 
   removeTodo(todoId: string): void {
     const { index } = this.findTodo(todoId);
-    this.todos.splice(index, index);
+    this.todos.splice(index, 1);
   }
 }
