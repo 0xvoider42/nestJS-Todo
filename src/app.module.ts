37f6b4d48b8acddb89b2ee35c6b11caeba6d882b
0xVoider42 <shautidzenikoloz@gmail.com>
2022-10-18 16:50:45 +0300

    adds pino-pretty

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,18 @@ import { TodoModule } from './todo/todo.module';
 import { LoggerModule } from 'nestjs-pino';
 
 @Module({
-  imports: [LoggerModule.forRoot(), TodoModule],
+  imports: [
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            singleLine: true,
+          },
+        },
+      },
+    }),
+    TodoModule,
+  ],
 })
 export class AppModule {}
