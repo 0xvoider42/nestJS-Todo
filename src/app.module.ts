8df8ecaafba0b2e853950e3ca1ce5fc7ae12b9bd
0xVoider42 <shautidzenikoloz@gmail.com>
2022-10-19 11:52:35 +0300

    addresses comments

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,27 @@
 import { Module } from '@nestjs/common';
 import { TodoModule } from './todo/todo.module';
 import { LoggerModule } from 'nestjs-pino';
+import { randomUUID } from 'crypto';
+import { TodoService } from './todo/todo.service';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 
 @Module({
   imports: [
     LoggerModule.forRoot({
       pinoHttp: {
+        autoLogging: false,
         transport: {
           target: 'pino-pretty',
           options: {
             singleLine: true,
+            genReqId: randomUUID(),
           },
         },
       },
     }),
     TodoModule,
   ],
+  providers: [{ provide: APP_INTERCEPTOR, useClass: LoggingInterceptor }],
 })
 export class AppModule {}
--- src/common/interceptors/logging.interceptor.ts
+++ src/common/interceptors/logging.interceptor.ts
@@ -13,29 +13,17 @@ export class LoggingInterceptor implements NestInterceptor {
   private readonly logger = new Logger(LoggingInterceptor.name);
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    this.logger.log('start interception');
-
     const request = context.switchToHttp().getRequest();
     const userAgent = request.get('user-agent' || '');
     const { ip, method, path: url } = request;
 
-    this.logger.log(
-      `${method} ${ip} ${url} ${userAgent} : ${
-        context.getClass().name
-      } have been invoked`,
-    );
-
-    const now = Date.now();
+    this.logger.log('Request inbound', { method, url, userAgent, ip });
 
     return next
       .handle()
       .pipe(
         tap(() =>
-          this.logger.log(
-            `${method} ${url} ${userAgent} ${ip} it took: ${
-              Date.now() - now
-            }ms to complete`,
-          ),
+          this.logger.log('Request outbound', { method, url, userAgent, ip }),
         ),
       );
   }
