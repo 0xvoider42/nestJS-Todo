a6d496f40d1c84c53bb7c85c6c2a57556318aec9
0xVoider42 <shautidzenikoloz@gmail.com>
2022-11-22 15:16:59 +0200

    refactors

--- after-test.ts
+++ after-test.ts
@@ -3,7 +3,7 @@ import { execSync } from 'child_process';
 module.exports = () => {
   try {
     console.log('DB Has stopped');
-    execSync('docker compose -f test.docker-compose.yml down');
+    execSync('docker compose -f docker-compose.test.yml down');
   } catch (error) {
     console.log(error);
   }
--- pre-test.ts
+++ pre-test.ts
@@ -2,7 +2,7 @@ import { execSync } from 'child_process';
 
 module.exports = () => {
   try {
-    execSync('docker compose -f test.docker-compose.yml up -d');
+    execSync('docker compose -f docker-compose.test.yml up -d');
     execSync('npx mikro-orm migration:up');
   } catch (error) {
     console.log(error);
